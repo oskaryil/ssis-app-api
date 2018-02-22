35ebec7984b7c55e0f415b005ef8c45e60e05670
Oskar Yildiz <yildizoskar@gmail.com>
2018-02-22 21:51:06 +0100

    added route for fetching the full schedule and also added caching for it

--- src/routes/schedule.routes.js
+++ src/routes/schedule.routes.js
@@ -9,6 +9,7 @@ import { authJwt } from '../services/auth';
 
 const routes = new Router();
 
+routes.route('/').get(authJwt, ScheduleController.getFullSchedule);
 routes.route('/currentclass').get(authJwt, ScheduleController.getCurrentClass);
 
 export default routes;
