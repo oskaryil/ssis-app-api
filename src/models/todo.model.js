460f347829b19d185137818c0ac348f79ff34918
Oskar Yildiz <yildizoskar@gmail.com>
2018-08-06 14:40:22 +0200

    Created Todo Model

---
+++ src/models/todo.model.js
@@ -0,0 +1,32 @@
+import mongoose from 'mongoose';
+import uniqueValidator from 'mongoose-unique-validator';
+
+const TodoSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      min: 3,
+    },
+    dueDate: {
+      type: Date,
+      required: false,
+    },
+    done: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    createdBy: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+TodoSchema.plugin(uniqueValidator);
+
+export default mongoose.model('Todo', TodoSchema);
