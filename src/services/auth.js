16a1eeceede975378676143889b817c51c75b7a3
Oskar Yildiz <yildizoskar@gmail.com>
2018-08-06 14:48:44 +0200

    removed unused import of request

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,7 +1,6 @@
 import passport from "passport";
 import LocalStrategy from "passport-local";
 import { Strategy as JWTStrategy, ExtractJwt } from "passport-jwt";
-import request from "superagent";
 
 import User from "../models/user.model";
 import constants from "../config/constants";
